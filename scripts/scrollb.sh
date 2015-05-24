897bd51e0222376009b461a0eb764e82892cb4a9
jballard1991 <username@users.noreply.github.com>
2015-05-23 23:16:45 -0700

    Added descriptive comment

--- scripts/scrollb.sh
+++ scripts/scrollb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Writes 4chan.org /b/ to the screen as text with some filters
 while true; do links -dump 4chan.org/b/|grep -Ev '___| / |IFrame|Start a New|Bottom Cata|Advertise|Dt Setting|Link: can|4chan Pass|Login|Please read the Rul|Yotsuba|Futaba|Burichan|Photon|Delete Post:|Show All'|sed 's/art/aut/g'|baud -900; done
