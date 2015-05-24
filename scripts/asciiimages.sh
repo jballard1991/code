c3ea8eafc1d35f784fbb206df8286db4094e0c89
jballard1991 <username@users.noreply.github.com>
2015-05-24 00:09:02 -0700

    Added verbosity

--- scripts/asciiimages.sh
+++ scripts/asciiimages.sh
@@ -1,1 +1,1 @@
-while true; do for i in `ls *.jpg `; do jp2a $i;rm -v $i;sleep 1; done; for i in `ls *.png`; do echo $i;convert $i `echo $i| sed 's/\.png/\.jpg/g'`;rm -v $i; done; done
+while true; do for i in `ls *.jpg `; do jp2a $i;rm -v $i;sleep 1; done; for i in `ls *.png`; do echo "converting $i";convert $i `echo $i| sed 's/\.png/\.jpg/g'`;rm -v $i; done; done
