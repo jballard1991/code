55c3d1d2c76a1e516df6dd37bc0550ef5585c953
jballard1991 <username@users.noreply.github.com>
2015-05-25 16:55:47 -0700

    "ui" change, displays some more info

--- pi/read.pibin.as.ascii.sh
+++ pi/read.pibin.as.ascii.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#read file containing binary represented as ascii 0 and 1, convert to ascii, read with xxd, iterate every 5 seconds
-while true ; do cat file| sed 's/[2-9]//g' | perl -lpe '$_=pack"B*",$_' | xxd -c 48 ; echo "[*] ==> reading again... " ; sleep 5; done
+#read file containing binary represented as ascii 0 and 1, convert to ascii, read with xxd, iterate every 15 seconds
+while true ; do echo "[*] ==> reading "; cat file| sed 's/[2-9]//g' | perl -lpe '$_=pack"B*",$_' | xxd -c 48 ; echo -n "pi digits: "; cat file|wc -c;echo -n "pibin digits: ";cat file | sed 's/[2-9]//g' | wc -c;sleep 15; done
